cd59c5c6a2c01f9125f4eda40b62fc2313d3e3e4
Kaylla Son <kaylla@Kayllas-MacBook-Air.local>
2023-05-30 15:24:46 +0900

    Modify: 초기세팅수정

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Nav() {
+  return <h1>Nav</h1>;
+}
+
+export default Nav;
