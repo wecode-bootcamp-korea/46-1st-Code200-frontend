8a97a332f12b0657c3a72fde0647951f0c96038c
minjeong <hn01472@gmail.com>
2023-05-30 23:05:14 +0900

    feature/main-new

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import './Main.scss';
 
 function Main() {
-  return <div>Main</div>;
+  return (
+    <div>
+      <div className="slide">slide</div>
+      <div className="weekly">weekly best</div>
+      <div className="newPrd">
+        <div className="title">신메뉴</div>
+        <a href="@@">
+          <img
+            className="newBev"
+            src="https://github.com/dxxcw/code200-images/blob/main/images/Main/new.png?raw=true"
+            alt="newprd"
+          />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
