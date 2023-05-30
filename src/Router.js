cd7a8aa48f9ac9d1813e0f247905da7bdf505304
Kaylla Son <kaylla@Kayllas-MacBook-Air.local>
2023-05-30 16:25:44 +0900

    Modify: 초기세팅수정

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main/Main';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+import ProductList from './pages/ProductList/ProductList';
+import ProductDetail from './pages/ProductDetail/ProductDetail';
+import Cart from './pages/Cart/Cart';
+import Order from './pages/Order/Order';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 
@@ -8,7 +15,13 @@ const Router = () => {
     <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/product-list" element={<ProductList />} />
+        <Route path="/product-detail" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<Order />} />
       </Routes>
       <Footer />
     </BrowserRouter>
