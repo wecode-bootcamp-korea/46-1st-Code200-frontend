3987d46df2d873380584b2a73af6545cc871b3fc
minjeong <hn01472@gmail.com>
2023-05-31 10:17:50 +0900

    ADD : 신메뉴 이미지 구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -8,13 +8,33 @@ function Main() {
       <div className="weekly">weekly best</div>
       <div className="newPrd">
         <div className="title">신메뉴</div>
-        <a href="@@">
-          <img
-            className="newBev"
-            src="https://github.com/dxxcw/code200-images/blob/main/images/Main/new.png?raw=true"
-            alt="newprd"
-          />
-        </a>
+        <div className="newWrap">
+          <div className="newBevWrap">
+            <a href="@@">
+              <img
+                className="newBev"
+                src="https://github.com/dxxcw/code200-images/blob/main/images/Main/new.png?raw=true"
+                alt="newbv"
+              />
+            </a>
+            <div className="description">
+              <p> 2023 WEDIYA SUMMER SEASON | 리얼 생 초콜릿 라떼</p>
+            </div>
+          </div>
+
+          <div className="newFdWrap">
+            <a href="@@">
+              <img
+                className="newFood"
+                src="https://github.com/dxxcw/code200-images/blob/main/images/Main/newFood.png?raw=true"
+                alt="newfd"
+              />
+            </a>
+            <div className="description">
+              <p> 2023 WEDIYA'S NEW CHOICE | 스모어 모어 쿠키</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
