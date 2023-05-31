8c1ca3a2f1d047deaa6b7ee5e9c2b808e4d9a9ec
Maple <jinu@Mapleui-MacBook-Pro.local>
2023-05-31 17:42:34 +0900

    feature/footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,64 @@
 import React from 'react';
+import '../Footer/Footer.scss';
+import youtubeimg from '/Users/jinu/Desktop/46-1st-Code200-frontend/src/assets/image/youtube_logo.jpg';
+import instagramimg from '/Users/jinu/Desktop/46-1st-Code200-frontend/src/assets/image/instagram.jpg';
+import facebookimg from '/Users/jinu/Desktop/46-1st-Code200-frontend/src/assets/image/facebook.jpg';
 
 function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <footer>
+      <nav className="all">
+        <div className="wrapallright1">
+          <a href="##">
+            <p>브랜드소개</p>
+          </a>
+          <a href="##">
+            <p>오프라인 매장안내</p>
+          </a>
+          <a href="##">
+            <p>개인정보처리방침</p>
+          </a>
+          <a href="##">
+            <p>공지사항</p>
+          </a>
+          <a href="##">
+            <p>회원혜택</p>
+          </a>
+          <a href="##">
+            <p>자주묻는질문 FAQ</p>
+          </a>
+        </div>
+        <p className="phonenumber">1234-5678</p>
+        <div className="wrapall">
+          <p className="wrapalltop">
+            <h1>상담시간 : 오전 10시-오후5시 (토요일, 공휴일 휴무)</h1>
+            <h2>email : maplesyrupdd99@gmail.com(단체주문 문의)</h2>
+            <h3>email : importboss0917@naver.com (마케팅 협찬 문의)</h3>
+          </p>
+          <p className="wrapallmid">
+            <h4>주식회사 WEDIYA </h4>
+            <h5>사업자등록번호 : 123-45-67890</h5>
+            <h6>
+              사옥 주소로 상품을 발송할시에 교환 환불이 불가하므로, 사이트내에서
+              신청해주시기 바랍니다
+            </h6>
+          </p>
+        </div>
+        <div className="underline" />
+        <div className="uibutton">
+          <a href="##">
+            <img className="ui" src={youtubeimg} alt="youtube_image" />
+          </a>
+          <a href="##">
+            <img className="ui" src={instagramimg} alt="instagram_image" />
+          </a>
+          <a href="##">
+            <img className="ui" src={facebookimg} alt="facebook_image" />
+          </a>
+        </div>
+      </nav>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -36,44 +36,42 @@ const Login = () => {
   };
 
   return (
-    <>
-      <div className="login">
-        <p className="name">로그인</p>
-        <div className="wrap">
-          <p className="idcontent">이메일 아이디</p>
-          <input
-            type="text"
-            className="id"
-            placeholder="이메일을 입력해 주세요."
-            value={email}
-            onChange={handleEmailChange}
-          />
-          <p className="passwordcontent">비밀번호 </p>
-          <input
-            type="password"
-            className="password"
-            placeholder="비밀번호를 입력해 주세요."
-            value={pw}
-            onChange={handlePwChange}
-            onKeyDown={handleOnKeyPress}
-          />
-          <button
-            type="button"
-            className="loginbutton"
-            style={{ backgroundColor: isValid ? '#FFA500' : '#42A5F5' }}
-            onClick={goMain}
-          >
-            로그인
-          </button>
-        </div>
-        <div className="findid">
-          <Link to="/">아이디/비밀번호 찾기</Link>
-        </div>
-        <div className="signup">
-          <Link to="/">이메일로 회원가입</Link>
-        </div>
+    <div className="login">
+      <p className="name">로그인</p>
+      <div className="wrap">
+        <p className="idcontent">이메일 아이디</p>
+        <input
+          type="text"
+          className="id"
+          placeholder="이메일을 입력해 주세요."
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <p className="passwordcontent">비밀번호 </p>
+        <input
+          type="password"
+          className="password"
+          placeholder="비밀번호를 입력해 주세요."
+          value={pw}
+          onChange={handlePwChange}
+          onKeyDown={handleOnKeyPress}
+        />
+        <button
+          type="button"
+          className="loginbutton"
+          style={{ backgroundColor: isValid ? '#FFA500' : '#42A5F5' }}
+          onClick={goMain}
+        >
+          로그인
+        </button>
       </div>
-    </>
+      <div className="findid">
+        <Link to="/">아이디/비밀번호 찾기</Link>
+      </div>
+      <div className="signup">
+        <Link to="/">이메일로 회원가입</Link>
+      </div>
+    </div>
   );
 };
 
