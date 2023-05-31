91dce4448403f8b6b37b9a7a51fe183bae532d57
minjeong <hn01472@gmail.com>
2023-05-31 16:58:12 +0900

    "feature/signup" - 이용약관 ui 구현 - 체크박스 구현

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import './Signup.scss';
+import AGREEMENT_LIST from './Agreemnet';
 
 function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div className="signup">
+      {AGREEMENT_LIST.map(list => (
+        <>
+          <div className="itemFlex">
+            <h2 key={list.id} className="agreementTitle">
+              {list.title} <span>{list.span}</span>
+            </h2>
+            <p>
+              <label className="check">
+                <input type="checkbox" />
+                <span className="icon" />
+              </label>
+            </p>
+          </div>
+          <div className="agree">
+            <div className="agreeScroll">{list.content}</div>
+          </div>
+        </>
+      ))}
+    </div>
+  );
 }
 
 export default Signup;
