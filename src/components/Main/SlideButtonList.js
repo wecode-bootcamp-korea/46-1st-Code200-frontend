adb0cb5f47ab37d103081b81af26ee39b2beda22
minjeong <hn01472@gmail.com>
2023-06-01 20:31:22 +0900

    MODIFY: mock데이터 수정완료

--- src/components/Main/SlideButtonList.js
+++ src/components/Main/SlideButtonList.js
@@ -2,26 +2,18 @@ const SLIDE_BUTTON_LIST = [
   {
     id: 1,
     idName: 'slide01',
-    onclick: '{() => setChecked(1)}',
-    checked: '{checked === 1}',
   },
   {
     id: 2,
     idName: 'slide02',
-    onclick: '{() => setChecked(2)}',
-    checked: '{checked === 2}',
   },
   {
     id: 3,
     idName: 'slide03',
-    onclick: '{() => setChecked(3)}',
-    checked: '{checked === 3}',
   },
   {
     id: 4,
     idName: 'slide04',
-    onclick: '{() => setChecked(4)}',
-    checked: '{checked === 4}',
   },
 ];
 
