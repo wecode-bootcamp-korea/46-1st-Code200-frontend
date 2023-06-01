0a292e91ad9ce88d026782c439aeaa97fb003426
minjeong <hn01472@gmail.com>
2023-06-01 11:37:26 +0900

    MODIFY : map매서드로 UI간소화

--- src/components/Main/New.js
+++ src/components/Main/New.js
@@ -1,40 +1,30 @@
 import React from 'react';
 import './New.scss';
+import NEW_LIST from './NewList';
 
 function New() {
   return (
     <div>
       <div className="slide">slide</div>
       <div className="weekly">weekly best</div>
+
+      {/* 신메뉴 Wrap */}
       <div className="newPrd">
         <div className="title">신메뉴</div>
-
         <div className="newWrap">
-          <div className="newBevWrap">
-            <div className="imgWrapper">
-              <img
-                className="newBev"
-                src="https://github.com/dxxcw/code200-images/blob/main/images/Main/new.png?raw=true"
-                alt="newbv"
-              />
+          {/* 신메뉴 이미지 */}
+          {NEW_LIST.map(newList => (
+            <div key={newList.id} className={newList.classWarp}>
+              <div className="imgWrapper">
+                <img
+                  className={newList.name}
+                  src={newList.src}
+                  alt={newList.alt}
+                />
+              </div>
+              <p className="description">{newList.description}</p>
             </div>
-            <p className="description">
-              2023 WEDIYA SUMMER SEASON | 리얼 생 초콜릿 라떼
-            </p>
-          </div>
-
-          <div className="newFdWrap">
-            <div className="imgWrapper">
-              <img
-                className="newFood"
-                src="https://github.com/dxxcw/code200-images/blob/main/images/Main/newFood.png?raw=true"
-                alt="newfd"
-              />
-            </div>
-            <p className="description">
-              2023 WEDIYA'S NEW CHOICE | 스모어 모어 쿠키
-            </p>
-          </div>
+          ))}
         </div>
       </div>
     </div>
