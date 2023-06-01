0aa1c06f5f4aa3b82de64e7cb362a6eafd589924
shfkd123 <shfkd123@gmail.com>
2023-06-02 03:34:44 +0900

    FIX : 반복되는 UI map 메서드 활용

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,73 +1,81 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './ProductDetail.scss';
+import { DETAIL_INFO } from '../UiData/productDetailUiData';
+import { DETAIL_NOTE } from '../UiData/productDetailNote';
 
 function ProductDetail() {
-  const [productDetail, setProductDetail] = useState([]);
+  const [productDetail, setProductDetail] = useState({});
+
+  let categoryArr = [
+    {
+      id: '3',
+      value: productDetail.name,
+    },
+    {
+      id: '2',
+      value: productDetail.subcategory,
+    },
+    {
+      id: '1',
+      value: productDetail.category,
+    },
+  ];
+
   useEffect(() => {
-    fetch('http://10.58.52.145:8000/products/4', { method: 'GET' })
+    // fetch('http://10.58.52.145:8000/products/7', { method: 'GET' });
+    fetch('data/productDetail.json')
       .then(res => res.json())
-      .then(res => {
-        setProductDetail(res);
+      .then(data => {
+        console.log(data);
+        setProductDetail(data.data[0]);
       });
   }, []);
+
   return (
     <div className="productDetail">
       <div className="productContainer">
         <div className="productDetailImg">
           <div className="detailTopImg">
             <ul className="detailTopImgUl">
-              <li className="detailTopImgli">
-                <img
-                  className="bestItemImg"
-                  src="https://raw.githubusercontent.com/dxxcw/code200-images/minji_images/images/Main/coffe-bag/jon-tyson-KRedbshBxEk-unsplash.jpg"
-                  alt="bestitem"
-                />
-                <img
-                  className="bestItemImg"
-                  src="https://raw.githubusercontent.com/dxxcw/code200-images/minji_images/images/Main/icon/photo-1562051036-e0eea191d42f.avif"
-                  alt="bestitem"
-                />
-                <img
-                  className="bestItemImg"
-                  src="https://raw.githubusercontent.com/dxxcw/code200-images/minji_images/images/Main/icon/photo-1541469406036-71229832e06e.avif"
-                  alt="bestitem"
-                />
-                <img
-                  className="bestItemImg"
-                  src="https://raw.githubusercontent.com/dxxcw/code200-images/minji_images/images/Main/icon/photo-1573628684835-ca186702bbde.avif"
-                  alt="bestitem"
-                />
-              </li>
+              {/* {productDetail.image.map(productDetailImg => {
+                return (
+                  <li className="detailTopImgli" key={productDetailImg.index}>
+                    <img
+                      className="bestItemImg"
+                      src={productDetailImg.id}
+                      alt="bestitem"
+                    />
+                  </li>
+                );
+              })} */}
             </ul>
           </div>
         </div>
+
         <div className="productDetailInfo">
           <div className="productRoute">
-            <Link to="/" className="productRouteCategory">
-              <span>디카페인 하우스 블렌드 250g</span>
-            </Link>
-            <Link to="/" className="productRouteCategory">
-              <span>원두 </span>
-              <img
-                className="shareSns"
-                src="https://raw.githubusercontent.com/dxxcw/code200-images/minji_images/images/Main/icon/icon_arrow.png"
-                alt="shareSns"
-              />
-            </Link>
-            <Link to="/" className="productRouteCategory">
-              <span>커피 </span>
-              <img
-                className="shareSns"
-                src="https://raw.githubusercontent.com/dxxcw/code200-images/minji_images/images/Main/icon/icon_arrow.png"
-                alt="shareSns"
-              />
-            </Link>
+            {categoryArr.map((arr, index) => {
+              return (
+                <Link to="/" className="productRouteCategory" key={arr.id}>
+                  <span className="productSpan">{arr.value}</span>
+                  {index !== categoryArr.length - 1 && (
+                    <img
+                      className="shareSns"
+                      src="https://raw.githubusercontent.com/dxxcw/code200-images/minji_images/images/Main/icon/icon_arrow.png"
+                      alt="shareSns"
+                    />
+                  )}
+                </Link>
+              );
+            })}
           </div>
+
           <div className="productDetail">
             <div className="productTitle">
-              <span className="title">{productDetail.data[0].name}</span>
+              <span className="title">{productDetail.name}</span>
             </div>
+            {/* 태그 컴포넌트 */}
             <div className="productTag">
               <span className="freePrd">무료배송</span>
               <span className="redPrd">품절임박</span>
@@ -75,7 +83,7 @@ function ProductDetail() {
             </div>
             <div className="productDetailInner">
               <div className="productAmount">
-                <span>{productDetail.data[0].price}</span>
+                <span>{productDetail.price}</span>
               </div>
               <div className="productShare">
                 <img
@@ -127,48 +135,32 @@ function ProductDetail() {
               <span className="productCountBig">총 상품 금액</span>
             </div>
             <div className="productAllCount">
-              <span className="productCountSmall">0 </span>
+              <span className="productCountSmall">{productDetail.price}</span>
               <span className="productCountBig">(0개)</span>
             </div>
           </div>
-          <div className="detailInfo">
-            <span className="detailInfoSpan">상품정보 제공공시</span>
-            <Link to="/" className="detailInfoLink">
-              <img
-                className="shareSns"
-                src="https://raw.githubusercontent.com/dxxcw/code200-images/minji_images/images/Main/icon/icon_arrow.png"
-                alt="shareSns"
-              />
-            </Link>
-          </div>
-          <div className="detailInfo">
-            <span className="detailInfoSpan">배송안내</span>
-            <Link to="/" className="detailInfoLink">
-              <img
-                className="shareSns"
-                src="https://raw.githubusercontent.com/dxxcw/code200-images/minji_images/images/Main/icon/icon_arrow.png"
-                alt="shareSns"
-              />
-            </Link>
-          </div>
-          <div className="detailInfo">
-            <span className="detailInfoSpan">취소/반품/교환/환불 안내</span>
-            <Link to="/" className="detailInfoLink">
-              <img
-                className="shareSns"
-                src="https://raw.githubusercontent.com/dxxcw/code200-images/minji_images/images/Main/icon/icon_arrow.png"
-                alt="shareSns"
-              />
-            </Link>
-          </div>
-          <span />
+          {DETAIL_INFO.map(detailInfo => {
+            return (
+              <div className="detailInfo" key={detailInfo.id}>
+                <span className="detailInfoSpan">{detailInfo.infoName}</span>
+                <Link to="/" className="detailInfoLink">
+                  <img
+                    className="shareSns"
+                    src="https://raw.githubusercontent.com/dxxcw/code200-images/minji_images/images/Main/icon/icon_arrow.png"
+                    alt="shareSns"
+                  />
+                </Link>
+              </div>
+            );
+          })}
         </div>
       </div>
+
       <div className="productInfo">
-        <div className="productDescArea">
-          <p className="productDescAreaPtag">
-            {productDetail.data[0].description}
-          </p>
+        <div className="productInfoInner">
+          <div className="productDescArea">
+            <p className="productDescAreaPtag">{productDetail.description}</p>
+          </div>
           <div className="productDescAreaImg">
             <img
               className="productDescImg"
@@ -181,27 +173,22 @@ function ProductDetail() {
               alt="productDescImg"
             />
           </div>
-          <div />
         </div>
+
         <div className="productNoticeTop">
-          <div className="productNotice">
-            <div className="productTastingNotes">
-              <span className="bigSpan">Tasting Notes</span>
-            </div>
-            <div className="productTastingNotesExp">
-              <span className="smallSpan">부드럽고 균형이 잡힘</span>
-            </div>
-          </div>
-          <div className="productNotice">
-            <div className="productEnjoyWith">
-              <span className="bigSpan">Enjoy with</span>
-            </div>
-            <div className="productEnjoyWithExp">
-              <span className="smallSpan">
-                요거트 한 컵과 조간신문과 함께 즐기세요.
-              </span>
-            </div>
-          </div>
+          {DETAIL_NOTE.map(note => {
+            return (
+              <div className="productNotice" key={note.id}>
+                <div className="productTastingNotes">
+                  <span className="bigSpan">{note.title}</span>
+                </div>
+
+                <div className="productTastingNotesExp">
+                  <span className="smallSpan">{note.content}</span>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
       <div>리뷰 컴포넌트</div>
---
+++ src/pages/UiData/productDetailNote.js
@@ -0,0 +1,12 @@
+export const DETAIL_NOTE = [
+  {
+    id: 0,
+    title: 'Tasting Notes',
+    content: '부드럽고 균형이 잡힘',
+  },
+  {
+    id: 1,
+    title: 'Enjoy with',
+    content: '요거트 한 컵과 조간신문과 함께 즐기세요.',
+  },
+];
