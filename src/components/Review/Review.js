b8409dce51f166b6153a0a8522d90429febef872
minjeong <hn01472@gmail.com>
2023-06-02 17:05:05 +0900

    ADD : 파일

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function Review() {
-  return <div>Review</div>;
+  return (
+    <div className="Review">
+      <div className="title">
+        <span>REVIEW</span>
+        <span>(total)</span>
+      </div>
+    </div>
+  );
 }
 
 export default Review;
