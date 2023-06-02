7ae04f1909a66657e3fdaacb7dbb1aeda538b228
shfkd123 <shfkd123@gmail.com>
2023-06-02 16:44:10 +0900

    FIX : 리뷰 수정사항 반영 및 css 수정

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -3,32 +3,35 @@ import { Link } from 'react-router-dom';
 import './ProductDetail.scss';
 import { DETAIL_INFO } from '../UiData/productDetailUiData';
 import { DETAIL_NOTE } from '../UiData/productDetailNote';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 function ProductDetail() {
   const [productDetail, setProductDetail] = useState({});
+  const [isImg, setIsImg] = useState(false);
 
-  let categoryArr = [
+  let CATEGORY_ARR = [
     {
       id: '3',
-      value: productDetail.name,
+      value: productDetail.product_name, // 스노우
     },
     {
       id: '2',
-      value: productDetail.subcategory,
+      value: productDetail.subcategory, // 커피
     },
     {
       id: '1',
-      value: productDetail.category,
+      value: productDetail.category, //음료
     },
   ];
 
   useEffect(() => {
-    // fetch('http://10.58.52.145:8000/products/7', { method: 'GET' });
+    // fetch('http://10.58.52.133:8000/products/7', { method: 'GET' })
     fetch('data/productDetail.json')
       .then(res => res.json())
       .then(data => {
-        console.log(data);
         setProductDetail(data.data[0]);
+        setIsImg(true);
       });
   }, []);
 
@@ -38,34 +41,29 @@ function ProductDetail() {
         <div className="productDetailImg">
           <div className="detailTopImg">
             <ul className="detailTopImgUl">
-              {/* {productDetail.image.map(productDetailImg => {
-                return (
-                  <li className="detailTopImgli" key={productDetailImg.index}>
-                    <img
-                      className="bestItemImg"
-                      src={productDetailImg.id}
-                      alt="bestitem"
-                    />
-                  </li>
-                );
-              })} */}
+              {isImg &&
+                productDetail.imageUrls.map(productDetailImg => {
+                  return (
+                    <li className="detailTopImgli" key={productDetailImg.index}>
+                      <img
+                        className="bestItemImg"
+                        src={productDetailImg.imageUrl}
+                        alt="bestItem"
+                      />
+                    </li>
+                  );
+                })}
             </ul>
           </div>
         </div>
 
         <div className="productDetailInfo">
           <div className="productRoute">
-            {categoryArr.map((arr, index) => {
+            {CATEGORY_ARR.map((arr, index) => {
               return (
                 <Link to="/" className="productRouteCategory" key={arr.id}>
                   <span className="productSpan">{arr.value}</span>
-                  {index !== categoryArr.length - 1 && (
-                    <img
-                      className="shareSns"
-                      src="https://raw.githubusercontent.com/dxxcw/code200-images/minji_images/images/Main/icon/icon_arrow.png"
-                      alt="shareSns"
-                    />
-                  )}
+                  {index !== 0 && <FontAwesomeIcon icon={faChevronRight} />}
                 </Link>
               );
             })}
@@ -73,7 +71,7 @@ function ProductDetail() {
 
           <div className="productDetail">
             <div className="productTitle">
-              <span className="title">{productDetail.name}</span>
+              <span className="title">{productDetail.product_name}</span>
             </div>
             {/* 태그 컴포넌트 */}
             <div className="productTag">
@@ -83,7 +81,7 @@ function ProductDetail() {
             </div>
             <div className="productDetailInner">
               <div className="productAmount">
-                <span>{productDetail.price}</span>
+                <span>{productDetail.product_price}</span>
               </div>
               <div className="productShare">
                 <img
@@ -101,11 +99,7 @@ function ProductDetail() {
             <span className="welcomeSpan">신규가입 혜택</span>
             <Link to="/" className="welcomeLink">
               신규 가입 시 웰컴 쿠폰팩 즉시 지급
-              <img
-                className="shareSns"
-                src="https://raw.githubusercontent.com/dxxcw/code200-images/minji_images/images/Main/icon/icon_arrow.png"
-                alt="shareSns"
-              />
+              <FontAwesomeIcon className="chevronRight" icon={faChevronRight} />
             </Link>
           </div>
           <div className="detailSize">
@@ -113,11 +107,7 @@ function ProductDetail() {
           </div>
           <div className="buttonArea">
             <button className="likeMove">
-              <img
-                className="shareSns"
-                src="https://raw.githubusercontent.com/dxxcw/code200-images/minji_images/images/Main/icon/detail_wish.png"
-                alt="shareSns"
-              />
+              <FontAwesomeIcon className="chevronRight" icon={faChevronRight} />
             </button>
             <button className="subCartMove">장바구니</button>
             <button className="buyMove">바로구매</button>
@@ -135,7 +125,9 @@ function ProductDetail() {
               <span className="productCountBig">총 상품 금액</span>
             </div>
             <div className="productAllCount">
-              <span className="productCountSmall">{productDetail.price}</span>
+              <span className="productCountSmall">
+                {productDetail.product_price}
+              </span>
               <span className="productCountBig">(0개)</span>
             </div>
           </div>
@@ -144,10 +136,9 @@ function ProductDetail() {
               <div className="detailInfo" key={detailInfo.id}>
                 <span className="detailInfoSpan">{detailInfo.infoName}</span>
                 <Link to="/" className="detailInfoLink">
-                  <img
-                    className="shareSns"
-                    src="https://raw.githubusercontent.com/dxxcw/code200-images/minji_images/images/Main/icon/icon_arrow.png"
-                    alt="shareSns"
+                  <FontAwesomeIcon
+                    className="chevronRight"
+                    icon={faChevronRight}
                   />
                 </Link>
               </div>
@@ -159,7 +150,9 @@ function ProductDetail() {
       <div className="productInfo">
         <div className="productInfoInner">
           <div className="productDescArea">
-            <p className="productDescAreaPtag">{productDetail.description}</p>
+            <p className="productDescAreaPtag">
+              {productDetail.product_description}
+            </p>
           </div>
           <div className="productDescAreaImg">
             <img
