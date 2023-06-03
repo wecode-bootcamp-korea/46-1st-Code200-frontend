2097c62de592f3f0abead8f538a72e08855242fc
minjeong <hn01472@gmail.com>
2023-06-03 22:38:53 +0900

    ADD : 글 작성 기능 추가

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,7 +2,34 @@ import React, { useEffect, useState } from 'react';
 import './Review.scss';
 
 function Review() {
+  const [inputReview, setInputReview] = useState('');
   const [review, setReview] = useState([]);
+
+  const handleReview = event => {
+    setInputReview(event.target.value);
+    console.log(inputReview);
+  };
+
+  function handleInputReview() {
+    const newReview = {
+      id: review.length + 1,
+      userInfo: 'WECODE@gmail.com',
+      review: inputReview,
+      rating: 5,
+    };
+
+    const updateReview = [newReview, ...review];
+    setReview(updateReview);
+    setInputReview('');
+
+    fetch('/data/reviewSample.json', {
+      method: 'POST',
+      body: JSON.stringify(updateReview),
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }
 
   useEffect(() => {
     fetch('/data/reviewSample.json', {
@@ -17,48 +44,35 @@ function Review() {
       <div className="reviewTitle">
         <span>REVIEW</span>
         <span>(total)</span>
-        <div className="submit">
-          <p>리뷰작성하기</p>
-        </div>
       </div>
 
       <div className="reviewSummary">
-        <div className="rating">
-          <div className="point">
-            <img src="/images/star.png" alt="별" />
-            <p className="start">5.0</p>
+        <div>
+          <p>고객 평점</p>
+          <div className="rating">
+            <div className="point">
+              <img src="/images/star.png" alt="별" />
+              <p className="start">5.0</p>
+            </div>
           </div>
         </div>
 
-        <div className="ratingScrore">
-          <div className="score">
-            <p>아주 좋아요</p>
-            <div className="scoreBar" />
+        <div className="submitReview">
+          <div className="submitRating">
+            <p>별점을 선택해주세요.</p>
+            <div className="starts">⭐️⭐️⭐️⭐️⭐️</div>
           </div>
-          <div className="score">
-            <p>맘에 들어요</p>
-            <div className="scoreBar" />
-          </div>
-          <div className="score">
-            <p>보통이에요</p>
-            <div className="scoreBar" />
-          </div>
-          <div className="score">
-            <p>그냥 그래요</p>
-            <div className="scoreBar" />
-          </div>
-          <div className="score">
-            <p>별로예요</p>
-            <div className="scoreBar" />
-          </div>
+          <input onChange={handleReview} className="reviewBox" type="text" />
+
+          <button onClick={handleInputReview}>등록하기</button>
         </div>
       </div>
+
       <div className="oneLineReview">
         {review.map(review => {
           return (
             <div key={review.id} className="oneLineWrap">
               <div>{review.rating}</div>
-
               <div className="userWrap">
                 <p className="oneLine">{review.review}</p>
                 <p className="userId">{review.userInfo}</p>
