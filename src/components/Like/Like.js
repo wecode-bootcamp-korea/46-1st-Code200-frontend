7e991e07b5686013b9bef4a67d3ddafc1c8ba745
Maple <jinu@Mapleui-MacBook-Pro.local>
2023-06-05 17:45:10 +0900

    Merge 완료

--- src/components/Like/Like.js
+++ src/components/Like/Like.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
 function Like() {
-  return <div>Like</div>;
-}
+  const LikeButton = () => {
+    const [count, setCount] = useState(0);
+    const [isLiked, setIsLiked] = useState(false);
+  
+    const handleLike = () => {
+      if (isLiked) {
+        setCount(count - 1);
+        setIsLiked(false);
+      } else {
+        setCount(count + 1);
+        setIsLiked(true);
+      }
+    };
+  
+    return (
+      <div>
+        <button onClick={handleLike}>
+          {isLiked ? 'Unlike' : 'Like'}
+        </button>
+        <span>{count}</span>
+      </div>
+    );
+  };
+  
+
+
 
 export default Like;
