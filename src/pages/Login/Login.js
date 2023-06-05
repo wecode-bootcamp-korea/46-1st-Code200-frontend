9fa49eb6b9495b084140fcfc8ac2bd8f837f5495
Maple <jinu@Mapleui-MacBook-Pro.local>
2023-06-05 17:06:45 +0900

    수정 및 색상css 이동

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -17,29 +17,25 @@ const Login = () => {
     setInputValues({ ...inputValues, [name]: value });
   };
 
-  const goMain = () => {
-    fetch('http://10.58.52.134:3000/users/signin', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json;charset=utf-8' },
-      body: JSON.stringify({
-        email: 'code200@gmail.com',
-        password: '!QWE123qwe',
-      }),
-    })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        if (isValid) {
-          navigate('/');
-        } else {
-          alert('틀린 비밀번호 입니다');
-        }
-      });
-  };
-
   const handleOnKeyPress = e => {
     if (e.key === 'Enter') {
       goMain();
+    }
+  };
+
+  const goMain = () => {
+    if (isValid) {
+      fetch('http://10.58.52.134:3000/users/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json;charset=utf-8' },
+        body: JSON.stringify(inputValues),
+      })
+        .then(res => res.json())
+        .then(data => {
+          navigate('/');
+        });
+    } else {
+      alert('잘못된 아이디나 비밀번호입니다');
     }
   };
 
@@ -51,6 +47,7 @@ const Login = () => {
         <input
           type="text"
           className="id"
+          name="email"
           placeholder="이메일을 입력해 주세요."
           value={inputValues.email}
           onChange={handleInput}
@@ -59,25 +56,21 @@ const Login = () => {
         <input
           type="password"
           className="password"
+          name="password"
           placeholder="비밀번호를 입력해 주세요."
           value={inputValues.password}
           onChange={handleInput}
           onKeyDown={handleOnKeyPress}
         />
-        <button
-          type="button"
-          className="loginbutton"
-          style={{ backgroundColor: isValid ? '#FFA500' : '#42A5F5' }}
-          onClick={goMain}
-        >
+        <button type="button" className="loginbutton" onClick={goMain}>
           로그인
         </button>
       </div>
       <div className="findid">
-        <Link to="/">아이디/비밀번호 찾기</Link>
+        <Link to="#">아이디/비밀번호 찾기</Link>
       </div>
       <div className="signup">
-        <Link to="/">이메일로 회원가입</Link>
+        <Link to="../Signup/Signup">이메일로 회원가입</Link>
       </div>
     </div>
   );
