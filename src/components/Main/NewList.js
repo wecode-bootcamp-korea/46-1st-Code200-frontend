b1efdafe400da45067526320b493af3a3fa3b56c
minjeong <hn01472@gmail.com>
2023-06-05 14:51:12 +0900

    ADD : main component

---
+++ src/components/Main/NewList.js
@@ -0,0 +1,19 @@
+const NEW_LIST = [
+  {
+    id: 1,
+    classWarp: 'newBevWrap',
+    name: 'newBev',
+    src: 'https://github.com/dxxcw/code200-images/blob/main/images/Main/new.png?raw=true',
+    alt: 'newbv',
+    description: '2023 WEDIYA SUMMER SEASON | 리얼 생 초콜릿 라떼',
+  },
+  {
+    id: 1,
+    classWarp: 'newFdWrap',
+    name: 'newFood',
+    src: 'https://github.com/dxxcw/code200-images/blob/main/images/Main/newFood.png?raw=true',
+    alt: 'newfd',
+    description: '2023 WEDIYA NEW CHOICE | 스모어 모어 쿠키',
+  },
+];
+export default NEW_LIST;
