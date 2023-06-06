d412a8ae721961b6ffd415f7b5fc2d705bb075be
Maple <jinu@Mapleui-MacBook-Pro.local>
2023-06-06 18:30:57 +0900

    로그인 네비게이트 및 fetch 확인

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -25,17 +25,21 @@ const Login = () => {
 
   const goMain = () => {
     if (isValid) {
-      fetch('http://10.58.52.134:3000/users/signin', {
+      fetch('http://10.58.52.86:8000/users/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json;charset=utf-8' },
         body: JSON.stringify(inputValues),
       })
         .then(res => res.json())
         .then(data => {
-          navigate('/');
+          if (data.message === 'USER_CORRECT') {
+            navigate('/');
+          } else {
+            alert('잘못된 아이디나 비밀번호입니다');
+          }
         });
     } else {
-      alert('잘못된 아이디나 비밀번호입니다');
+      alert('email과 비밀번호를 입력해주세요');
     }
   };
 
@@ -69,7 +73,7 @@ const Login = () => {
       <div className="findid">
         <a href="##">아이디/비밀번호 찾기</a>
       </div>
-      <div className="signup">
+      <div className="signUpp">
         <Link to="/signup">이메일로 회원가입</Link>
       </div>
     </div>
