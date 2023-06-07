ea0dc2adae0025bf512cf38a54534abac58968a6
minjeong <hn01472@gmail.com>
2023-06-07 15:07:47 +0900

    MODIFY : 슬라이드 이동 시간 변경

--- src/components/Main/WeeklyBest/WeeklyBest.js
+++ src/components/Main/WeeklyBest/WeeklyBest.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import ProductCard from '../../ProductCard/ProductCard';
-import { Link } from 'react-router-dom';
 import TAP_LIST from '../Data/TapList';
 import './WeeklyBest.scss';
 
