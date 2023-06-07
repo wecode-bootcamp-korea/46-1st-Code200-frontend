cb270d79e7856e5f4e8d6d8c86ca46d9173dab51
Maple <jinu@Mapleui-MacBook-Pro.local>
2023-06-07 17:47:33 +0900

    conflict resolve

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import Slide from '../../components/Main/Slide';
-import New from '../../components/Main/New';
 
 function Main() {
-  return (
-    <div>
-      <Slide />
-      <New />
-    </div>
-  );
+  return <div>Main</div>;
 }
 
 export default Main;
