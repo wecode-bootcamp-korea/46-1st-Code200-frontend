f228ac2922c240ca16c42f0bf7cbd7dcee8ba0fd
minjeong <hn01472@gmail.com>
2023-06-07 15:13:00 +0900

    MODIFY : 파일 경로 수정

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import Slide from '../../components/Main/Slide';
+import Slide from '../../components/Main/Slide/Slide';
 
-import New from '../../components/Main/New';
-import WeeklyBest from '../../components/Main/WeeklyBest';
+import New from '../../components/Main/New/New';
+import WeeklyBest from '../../components/Main/WeeklyBest/WeeklyBest';
 
 function Main() {
   return (
